6be2fa6b1e10cdb25774adf6b9bac09c15a9d0e7
Matheus Fernandes <88437505+MatheusFernandes2021@users.noreply.github.com>
2023-06-22 12:40:52 +0000

    #

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,5 +11,41 @@ import { IonicModule } from '@ionic/angular';
 })
 
 export class HomePage {
-  constructor() {}
+
+      // Funções calculadora
+      calc = {
+        num:"0",
+        prevNum:"",
+        result:"",
+    }
+
+    addNum(n:string){
+      let vrf = (this.calc.num == "0" && n != ".") 
+      this.calc.num  = (vrf ? n : this.calc.num + n);
+    }
+    
+    operation(op:string){
+        this.calc.prevNum += `${this.calc.num}${op}`;
+    }
+
+    invNum(){
+        this.calc.num = (this.calc.num[0] == "-" ? this.calc.num.slice(1):"-"+this.calc.num)
+    }
+    rmvNum(){
+        this.calc.num.slice(0,-1);
+    }
+    eql(){
+        this.calc.prevNum += this.calc.num;
+        this.calc.result = eval(this.calc.prevNum)
+    }
+
+    clear(){
+        this.calc.num="0";
+        this.calc.prevNum="";
+        this.calc.result="";
+    }
+
+    // att:(){
+
+    // }
 }
